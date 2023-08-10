bdea00b93bd82baa309a399c45c905965e409ad4
Nicholas Williams <williamsnick606@gmail.com>
2023-08-09 17:37:53 -0700

    initializing the Geeks4geeks navbar skeleton repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Events from './pages/Events.js';
+import AnnualReport from './pages/Annualreport.js';
+import Teams from './pages/Teams.js';
+import Blogs from './pages/Blogs.js';
+import SignUp from './pages/Signup.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={About} />
+		<Route path='/events' component={Events} />
+		<Route path='/annual' component={AnnualReport} />
+		<Route path='/team' component={Teams} />
+		<Route path='/blogs' component={Blogs} />
+		<Route path='/sign-up' component={SignUp} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
